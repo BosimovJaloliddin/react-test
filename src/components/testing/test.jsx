946e4f198a342fdcfe205c95abef972c0eb4b857
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-05-15 10:15:27 +0500

    styled-components

--- src/components/testing/test.jsx
+++ src/components/testing/test.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "./test.css";
-// import { student } from "./mock";
-
+import { Container, Box, Title, Subtitle } from "./testStyled";
 class Test extends React.Component {
   render() {
     return (
       <>
-        <div></div>
+        <Container>
+          <Box bg="white" type="large">
+            Large
+          </Box>
+          <Box bg="red" type="medium">
+            Medium
+          </Box>
+          <Box bg="black" type="smoll">
+            Smoll
+          </Box>
+          <Title>Hi my bro</Title>
+          <Subtitle left>Hi my kro</Subtitle>
+          <Subtitle state="left">Hi my kro2</Subtitle>
+        </Container>
       </>
     );
   }
