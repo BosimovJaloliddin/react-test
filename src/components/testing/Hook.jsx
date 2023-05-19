0e375670f4a5f5e55795185c4dfb1ef4e7fc5177
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-05-20 02:39:26 +0500

    useReducer

--- src/components/testing/Hook.jsx
+++ src/components/testing/Hook.jsx
@@ -3,28 +3,29 @@ import React, { useReducer } from "react";
 const Hooks = () => {
   const reduce = (state, action) => {
     switch (action.type) {
-      case "counter":
+      case "bir":
         return state + action.payload;
       default:
         return state;
     }
   };
   const [count, dispatch] = useReducer(reduce, 0);
-
   return (
     <div>
-      <h1>Counter:{count}</h1>
-      <button onClick={() => dispatch({ type: "counter", payload: 1 })}>
-        +
+      <h1>useReducer counter:{count}</h1>
+      <button onClick={() => dispatch({ type: "bir", payload: 1 })}>+1</button>
+      <button onClick={() => dispatch({ type: "bir", payload: 5 })}>+5</button>
+      <button onClick={() => dispatch({ type: "bir", payload: 20 })}>
+        +20
       </button>
-      <button onClick={() => dispatch({ type: "counter", payload: -1 })}>
-        -
-      </button>
-      <button onClick={() => dispatch({ type: "counter", payload: 50 })}>
+      <button onClick={() => dispatch({ type: "bir", payload: 50 })}>
         +50
       </button>
-      <button onClick={() => dispatch({ type: "counter", payload: -50 })}>
+      <button onClick={() => dispatch({ type: "bir", payload: -50 })}>
         -50
+      </button>
+      <button onClick={() => dispatch({ type: "bir", payload: 100 })}>
+        100
       </button>
     </div>
   );
