4bf6e4653dfe299c65b60fe4e605e6f535c6526e
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-05-20 01:54:55 +0500

    Hooks and useState

--- src/components/testing/Class.jsx
+++ src/components/testing/Class.jsx
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
 
-export default class Class extends Component {
+class Class extends Component {
   constructor() {
     super();
     this.state = {
       count: 0,
+      name: "Ali",
     };
   }
   render() {
     return (
       <div style={{ flex: 1 }}>
-        <h1>Class count:{this.state.count}</h1>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+        <h1>Counter:{this.state.count}</h1>
+        <button onClick={() => this.setState({ count: this.state.count - 1 })}>
           -
         </button>
-        <button onClick={() => this.setState({ count: this.state.count - 1 })}>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
           +
         </button>
       </div>
     );
   }
 }
+export default Class;
--- src/components/testing/Hooks.jsx
+++ src/components/testing/Hooks.jsx
@@ -1,48 +1,29 @@
 import React, { useState } from "react";
 const Hooks = () => {
+  // const [count, setCount] = useState(0);
+  // const [name, setName] = useState("Jaloliddin");
   const [data, setData] = useState({
-    one: 0,
-    two: 0,
-    there: 0,
+    count: 0,
+    name: "Jaloliddin",
   });
-  console.log(data);
-  const onPlus = ({ target: { name } }) => {
-    setData({ ...data, [name]: data[name] + 1 });
-  };
-  const onMinus = ({ target: { name } }) => {
-    setData({ ...data, [name]: data[name] - 1 });
-    console.log();
-  };
   return (
     <div style={{ flex: 1 }}>
-      <h1>
-        <button name="one" onClick={onMinus}>
-          -
-        </button>
-        {data.one}
-        <button name="one" onClick={onPlus}>
-          +
-        </button>
-      </h1>
-      <h1>
-        <button name="two" onClick={onMinus}>
-          -
-        </button>
-        {data.two}
-        <button name="two" onClick={onPlus}>
-          +
-        </button>
-      </h1>
-      <h1>
-        <button name="there" onClick={onMinus}>
-          -
-        </button>
-        {data.there}
-        <button name="there" onClick={onPlus}>
-          +
-        </button>
-      </h1>
+      <h1>hooks counter:{data.count}</h1>
+      <h1>hooks counter:{data.name}</h1>
+      <input
+        onChange={({ target: { value } }) => setData({ ...data, name: value })}
+        type="text"
+        value={data.name}
+      />{" "}
+      <br />
+      <button onClick={() => setData({ ...data, count: data.count - 1 })}>
+        -
+      </button>
+      <button onClick={() => setData({ ...data, count: data.count + 1 })}>
+        +
+      </button>
     </div>
   );
 };
+
 export default Hooks;
