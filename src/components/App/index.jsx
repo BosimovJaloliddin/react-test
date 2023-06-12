80fee7b34387947b9d6e73ed03dc34b485c0ba21
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-06-12 16:23:00 +0500

    ..

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,18 +1,7 @@
 import React from "react";
-const Heading = () => {
-  return <h1>HEllow world</h1>;
-};
-const Test = () => {
-  return <input type="text" placeholder="login" />;
-};
 
 const App = () => {
-  return (
-    <div>
-      <Heading />
-      <Test />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
