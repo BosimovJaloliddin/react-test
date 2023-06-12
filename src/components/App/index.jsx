485f39ca2950329d156447055ec80ca902c416cb
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-06-12 22:47:52 +0500

    ..

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,42 @@
+import { Carousel } from "antd";
 import React from "react";
+// import {} "../../img/img1.jpg";
+// import {Image2} "../../img/img2.jpg";
 
-const App = () => {
-  return <div>App</div>;
+const contentStyle = {
+  height: "360px",
+  color: "#fff",
+  lineHeight: "260px",
+  textAlign: "center",
+  background: "#364d79",
 };
-
+const App = () => (
+  <div style={{ height: "300px" }}>
+    <Carousel autoplay>
+      <div>
+        <h3 style={contentStyle}>
+          <img style={{ width: "100%" }} src="./img/img1.jpg" alt="" />
+        </h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>
+          <img style={{ width: "100%" }} src="./img/img2.jpg" alt="" />
+        </h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>
+          <img style={{ width: "100%" }} src="./img/img1.jpg" alt="" />
+        </h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>
+          <img style={{ width: "100%" }} src="./img/img2.jpg" alt="" />
+        </h3>
+      </div>
+    </Carousel>
+    <div>
+      <h1 style={{ color: "red" }}>Hellow</h1>
+    </div>
+  </div>
+);
 export default App;
